ad619cb0e0a970531be24f16dc48ae2d395e5d0b
Leonid Kogan <leonsio@users.noreply.github.com>
2026-08-14 11:16:44 +0200

    Add integration constants

---
+++ custom_components/kfg_vertretungsplan/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "kfg_vertretungsplan"
+CONF_BASE_URL = "base_url"
+CONF_SCAN_INTERVAL = "scan_interval"
+DEFAULT_BASE_URL = "http://vertretung.kaiserin-friedrich.de"
+DEFAULT_SCAN_INTERVAL = 300
+PLATFORMS = ["sensor"]
