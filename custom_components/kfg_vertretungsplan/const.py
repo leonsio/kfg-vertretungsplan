504b88bc3b939b58c8b3a92426887147098a0b5b
Leonid Kogan <leonsio@users.noreply.github.com>
2026-09-05 22:51:26 +0200

    Add class configuration constant

--- custom_components/kfg_vertretungsplan/const.py
+++ custom_components/kfg_vertretungsplan/const.py
@@ -1,6 +1,7 @@
 DOMAIN = "kfg_vertretungsplan"
 CONF_BASE_URL = "base_url"
 CONF_SCAN_INTERVAL = "scan_interval"
+CONF_CLASS = "class_name"
 DEFAULT_BASE_URL = "http://vertretung.kaiserin-friedrich.de"
 DEFAULT_SCAN_INTERVAL = 300
 PLATFORMS = ["sensor"]
